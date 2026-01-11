9a90e4368e8b008b42a3b479630d7e02a0af4341
Brian Rotama Putra <brianrotamaputra@192.168.1.9>
2026-01-11 19:13:14 +0700

    add no cache get from gsheet

--- tests/login-logout.spec.ts
+++ tests/login-logout.spec.ts
@@ -1,23 +1,28 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage.js';
 import { SecurePage } from '../pages/SecurePage.js';
+import { readCsvFromUrl } from '../utils/csvFromUrl.js';
 
 test('Login → Logout flow', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const securePage = new SecurePage(page);
 
+  // 🔹 Ambil data dari Google Sheet (SELALU FRESH)
+  const users = await readCsvFromUrl(process.env.GSHEET_URL!);
+  const { username, password } = users[0];
+
   // 1️⃣ Login
   await loginPage.goto();
-  await loginPage.login('tomsmith', 'SuperSecretPassword!');
+  await loginPage.login(username, password);
 
-  // Assert berhasil login
+  // 2️⃣ Assert berhasil login
   await securePage.isAt();
   await expect(securePage.flashMessage)
     .toContainText('You logged into a secure area!');
 
-  // 2️⃣ Logout (via UI, karena kita masih di /secure)
+  // 3️⃣ Logout
   await securePage.logout();
 
-  // Assert kembali ke login page
-  await expect(page).toHaveURL(/login/);
+  // 4️⃣ Assert kembali ke login page
+  await expect(page).toHaveURL(/\/login$/);
 });
